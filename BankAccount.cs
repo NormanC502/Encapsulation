ac2a59541fdc5fe56aac040bb3ae3cc7d3477a49
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-09 17:41:58 -0500

    finished

---
+++ BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+
+        public BankAccount() 
+        {
+        }
+
+        private double balance = 0; // Private field no use for get; or set; they are used in properties
+
+        //Encapsulation method (hiding information)
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        // Will return the amount stored in balance
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+
+    }             
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // explicit type would be BankAccount = new BankAccount();
+            var account = new BankAccount(); // inferred type
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you!   Your balance is now ${account.GetBalance()}");
+        }
+    }
+}
